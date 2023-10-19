313593f001361c86d8f5fcc73be48db138c0e90c
Ravonzz774 <vovash174@gmail.com>
2023-10-19 12:58:15 +0500

    first commit

---
+++ js/tailwind.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        white: "#ffffff",
+        black: "#1f1f1f",
+      },
+      fontFamily: { "unbound": "Unbounded" },
+    },
+
+    screens: {
+      sm: "280px",
+      md: "1320px",
+      lg: "1320px",
+    },
+  },
+};
